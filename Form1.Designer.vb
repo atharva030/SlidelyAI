40930a6424c744877db0640f4db36c519e265fed
atharva030 <uniflik.atharvajagdale@gmail.com>
2024-06-20 13:49:55 +0530

    Working branch

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -16,7 +16,7 @@ Partial Class Form1
         Button1.BackColor = Color.Khaki
         Button1.Location = New Point(157, 74)
         Button1.Name = "Button1"
-        Button1.Size = New Size(296, 40)
+        Button1.Size = New Size(335, 76)
         Button1.TabIndex = 0
         Button1.Text = "Create New Submissions (CTRL + N)"
         Button1.UseVisualStyleBackColor = False
@@ -24,9 +24,9 @@ Partial Class Form1
         ' Button2
         ' 
         Button2.BackColor = Color.DeepSkyBlue
-        Button2.Location = New Point(157, 132)
+        Button2.Location = New Point(157, 156)
         Button2.Name = "Button2"
-        Button2.Size = New Size(296, 43)
+        Button2.Size = New Size(335, 79)
         Button2.TabIndex = 1
         Button2.Text = "View Submission (CTRL + V)"
         Button2.UseVisualStyleBackColor = False
@@ -34,7 +34,7 @@ Partial Class Form1
         ' Label1
         ' 
         Label1.AutoSize = True
-        Label1.Location = New Point(145, 37)
+        Label1.Location = New Point(159, 38)
         Label1.Name = "Label1"
         Label1.Size = New Size(333, 20)
         Label1.TabIndex = 2
@@ -42,7 +42,7 @@ Partial Class Form1
         ' 
         ' Form1
         ' 
-        AutoScaleDimensions = New SizeF(8.0F, 20.0F)
+        AutoScaleDimensions = New SizeF(8F, 20F)
         AutoScaleMode = AutoScaleMode.Font
         ClientSize = New Size(751, 346)
         Controls.Add(Label1)
--- Form2.Designer.vb
+++ Form2.Designer.vb
@@ -5,133 +5,147 @@ Partial Class Form2
     Private components As System.ComponentModel.IContainer
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
-        TextBox1 = New System.Windows.Forms.TextBox()
-        TextBox2 = New System.Windows.Forms.TextBox()
-        TextBox3 = New System.Windows.Forms.TextBox()
-        Label1 = New System.Windows.Forms.Label()
-        Label2 = New System.Windows.Forms.Label()
-        Label3 = New System.Windows.Forms.Label()
-        ButtonSubmit = New System.Windows.Forms.Button()
-        Label4 = New System.Windows.Forms.Label()
-        Label5 = New System.Windows.Forms.Label()
-        TextBox4 = New System.Windows.Forms.TextBox()
-        ButtonToggleStopwatch = New System.Windows.Forms.Button()
-        LabelStopwatch = New System.Windows.Forms.Label()
-        Timer1 = New System.Windows.Forms.Timer(components)
+        components = New ComponentModel.Container()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        TextBox3 = New TextBox()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        ButtonSubmit = New Button()
+        Label4 = New Label()
+        Label5 = New Label()
+        TextBox4 = New TextBox()
+        ButtonToggleStopwatch = New Button()
+        LabelStopwatch = New Label()
+        Timer1 = New Timer(components)
         SuspendLayout()
         ' 
         ' TextBox1
         ' 
-        TextBox1.Location = New System.Drawing.Point(116, 50)
+        TextBox1.Location = New Point(251, 77)
+        TextBox1.Margin = New Padding(4, 5, 4, 5)
         TextBox1.Name = "TextBox1"
-        TextBox1.Size = New System.Drawing.Size(200, 20)
+        TextBox1.Size = New Size(265, 27)
         TextBox1.TabIndex = 1
         ' 
         ' TextBox2
         ' 
-        TextBox2.Location = New System.Drawing.Point(116, 90)
+        TextBox2.Location = New Point(251, 121)
+        TextBox2.Margin = New Padding(4, 5, 4, 5)
         TextBox2.Name = "TextBox2"
-        TextBox2.Size = New System.Drawing.Size(200, 20)
+        TextBox2.Size = New Size(265, 27)
         TextBox2.TabIndex = 2
         ' 
         ' TextBox3
         ' 
-        TextBox3.Location = New System.Drawing.Point(116, 130)
+        TextBox3.Location = New Point(251, 168)
+        TextBox3.Margin = New Padding(4, 5, 4, 5)
         TextBox3.Name = "TextBox3"
-        TextBox3.Size = New System.Drawing.Size(200, 20)
+        TextBox3.Size = New Size(265, 27)
         TextBox3.TabIndex = 3
         ' 
         ' Label1
         ' 
         Label1.AutoSize = True
-        Label1.Location = New System.Drawing.Point(30, 50)
+        Label1.Location = New Point(40, 77)
+        Label1.Margin = New Padding(4, 0, 4, 0)
         Label1.Name = "Label1"
-        Label1.Size = New System.Drawing.Size(38, 13)
+        Label1.Size = New Size(52, 20)
         Label1.TabIndex = 4
         Label1.Text = "Name:"
         ' 
         ' Label2
         ' 
         Label2.AutoSize = True
-        Label2.Location = New System.Drawing.Point(30, 90)
+        Label2.Location = New Point(40, 121)
+        Label2.Margin = New Padding(4, 0, 4, 0)
         Label2.Name = "Label2"
-        Label2.Size = New System.Drawing.Size(35, 13)
+        Label2.Size = New Size(49, 20)
         Label2.TabIndex = 5
         Label2.Text = "Email:"
         ' 
         ' Label3
         ' 
         Label3.AutoSize = True
-        Label3.Location = New System.Drawing.Point(30, 130)
+        Label3.Location = New Point(40, 168)
+        Label3.Margin = New Padding(4, 0, 4, 0)
         Label3.Name = "Label3"
-        Label3.Size = New System.Drawing.Size(61, 13)
+        Label3.Size = New Size(80, 20)
         Label3.TabIndex = 6
         Label3.Text = "Phone No.:"
         ' 
         ' ButtonSubmit
         ' 
-        ButtonSubmit.Location = New System.Drawing.Point(120, 220)
+        ButtonSubmit.BackColor = Color.DeepSkyBlue
+        ButtonSubmit.ForeColor = SystemColors.ControlText
+        ButtonSubmit.Location = New Point(40, 312)
+        ButtonSubmit.Margin = New Padding(4, 5, 4, 5)
         ButtonSubmit.Name = "ButtonSubmit"
-        ButtonSubmit.Size = New System.Drawing.Size(100, 23)
+        ButtonSubmit.Size = New Size(476, 35)
         ButtonSubmit.TabIndex = 7
         ButtonSubmit.Text = "Submit (Ctrl + S)"
-        ButtonSubmit.UseVisualStyleBackColor = True
+        ButtonSubmit.UseVisualStyleBackColor = False
         ' 
         ' Label4
         ' 
         Label4.AutoSize = True
-        Label4.Location = New System.Drawing.Point(30, 20)
+        Label4.Location = New Point(113, 31)
+        Label4.Margin = New Padding(4, 0, 4, 0)
         Label4.Name = "Label4"
-        Label4.Size = New System.Drawing.Size(212, 13)
+        Label4.Size = New Size(340, 20)
         Label4.TabIndex = 8
-        Label4.Text = "John Doe, Slidely Task 2 - Create Submission"
+        Label4.Text = "Athava Jagdale, Slidely Task 2 - Create Submission"
         ' 
         ' Label5
         ' 
         Label5.AutoSize = True
-        Label5.Location = New System.Drawing.Point(30, 170)
+        Label5.Location = New Point(40, 218)
+        Label5.Margin = New Padding(4, 0, 4, 0)
         Label5.Name = "Label5"
-        Label5.Size = New System.Drawing.Size(80, 13)
+        Label5.Size = New Size(155, 20)
         Label5.TabIndex = 10
         Label5.Text = "GitHub Link for Task 2:"
         ' 
         ' TextBox4
         ' 
-        TextBox4.Location = New System.Drawing.Point(116, 170)
+        TextBox4.Location = New Point(251, 215)
+        TextBox4.Margin = New Padding(4, 5, 4, 5)
         TextBox4.Name = "TextBox4"
-        TextBox4.Size = New System.Drawing.Size(200, 20)
+        TextBox4.Size = New Size(265, 27)
         TextBox4.TabIndex = 9
         ' 
         ' ButtonToggleStopwatch
         ' 
-        ButtonToggleStopwatch.Location = New System.Drawing.Point(30, 260)
+        ButtonToggleStopwatch.BackColor = Color.Khaki
+        ButtonToggleStopwatch.Location = New Point(40, 258)
+        ButtonToggleStopwatch.Margin = New Padding(4, 5, 4, 5)
         ButtonToggleStopwatch.Name = "ButtonToggleStopwatch"
-        ButtonToggleStopwatch.Size = New System.Drawing.Size(130, 23)
+        ButtonToggleStopwatch.Size = New Size(200, 35)
         ButtonToggleStopwatch.TabIndex = 11
         ButtonToggleStopwatch.Text = "Toggle Stopwatch (Ctrl + T)"
-        ButtonToggleStopwatch.UseVisualStyleBackColor = True
+        ButtonToggleStopwatch.UseVisualStyleBackColor = False
         ' 
         ' LabelStopwatch
         ' 
         LabelStopwatch.AutoSize = True
-        LabelStopwatch.Location = New System.Drawing.Point(200, 265)
+        LabelStopwatch.Location = New Point(251, 265)
+        LabelStopwatch.Margin = New Padding(4, 0, 4, 0)
         LabelStopwatch.Name = "LabelStopwatch"
-        LabelStopwatch.Size = New System.Drawing.Size(49, 13)
+        LabelStopwatch.Size = New Size(63, 20)
         LabelStopwatch.TabIndex = 12
         LabelStopwatch.Text = "00:00:00"
         ' 
         ' Timer1
         ' 
-        Timer1.Interval = 1000
-        AddHandler Timer1.Tick, AddressOf Timer1_Tick
         Timer1.Enabled = True
+        Timer1.Interval = 1000
         ' 
         ' Form2
         ' 
-        AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
-        AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        ClientSize = New System.Drawing.Size(350, 300)
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1171, 462)
         Controls.Add(LabelStopwatch)
         Controls.Add(ButtonToggleStopwatch)
         Controls.Add(Label5)
@@ -145,6 +159,7 @@ Partial Class Form2
         Controls.Add(TextBox2)
         Controls.Add(TextBox1)
         KeyPreview = True
+        Margin = New Padding(4, 5, 4, 5)
         Name = "Form2"
         Text = "Create Submission"
         ResumeLayout(False)
